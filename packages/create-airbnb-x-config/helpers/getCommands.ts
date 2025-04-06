fd8bd524f07b2465ae5321e3870545ad849ba315
Nisharg Shah <nishargshah3101@gmail.com>
2025-04-07 01:05:26 +0530

    feat: commands completed

--- packages/create-airbnb-x-config/helpers/getCommands.ts
+++ packages/create-airbnb-x-config/helpers/getCommands.ts
@@ -1,5 +1,5 @@
 import type { InstallPackagesArgs } from '@/helpers/installPackages';
-import { packageManagers } from '@/constants';
+import { configs, languages, packageManagers } from '@/constants';
 
 type GetCommands = (args: InstallPackagesArgs) => string[];
 
@@ -7,15 +7,43 @@ const getCommands: GetCommands = (args) => {
   const { typescript, language, config, packageManager } = args;
 
   const pmInstallationCommand = {
-    [packageManagers.NPM]: 'npm install -D',
-    [packageManagers.YARN]: 'yarn add -D',
-    [packageManagers.PNPM]: 'pnpm install -D',
+    [packageManagers.NPM]: 'npm install',
+    [packageManagers.YARN]: 'yarn add',
+    [packageManagers.PNPM]: 'pnpm install',
     [packageManagers.BUN]: 'bun add',
   };
 
-  const command = pmInstallationCommand[packageManager];
+  const commands = [
+    pmInstallationCommand[packageManager],
+    '-D',
+    'eslint',
+    'eslint-config-airbnb-extended',
+    'eslint-plugin-import-x',
+  ];
 
-  return [];
+  if (typescript) {
+    commands.push('eslint-import-resolver-typescript', 'typescript-eslint');
+  }
+
+  if (language === languages.OTHER) {
+    if (config.includes(configs.REACT)) {
+      commands.push('eslint-plugin-react', 'eslint-plugin-react-hooks', 'eslint-plugin-jsx-a11y');
+    }
+
+    if (config.includes(configs.NEXT)) {
+      commands.push('@next/eslint-plugin-next');
+    }
+  } else {
+    if (language === languages.REACT || language === languages.NEXT) {
+      commands.push('eslint-plugin-react', 'eslint-plugin-react-hooks', 'eslint-plugin-jsx-a11y');
+    }
+
+    if (language === languages.NEXT) {
+      commands.push('@next/eslint-plugin-next');
+    }
+  }
+
+  return commands;
 };
 
 export default getCommands;
