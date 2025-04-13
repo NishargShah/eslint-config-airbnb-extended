aab4c8336c21f81c2554c4d4498aa59fcc4cb02c
Nisharg Shah <nishargshah3101@gmail.com>
2025-04-13 16:39:30 +0530

    feat: created next ts and js config

---
+++ packages/create-airbnb-x-config/templates/next/ts/eslint.config.mjs
@@ -0,0 +1,44 @@
+import path from 'node:path';
+
+import { includeIgnoreFile } from '@eslint/compat';
+import { configs, plugins } from 'eslint-config-airbnb-extended';
+import { configs as tsConfigs } from 'typescript-eslint';
+
+export const projectRoot = path.resolve('.');
+export const gitignorePath = path.resolve(projectRoot, '.gitignore');
+
+const nextConfig = [
+  // React Plugin
+  plugins.react,
+  // React Hooks Plugin
+  plugins.reactHooks,
+  // React JSX-A11y Plugin
+  plugins.reactA11y,
+  // Airbnb Next Recommended Config
+  ...configs.next.recommended,
+];
+
+const typescriptConfig = [
+  // Airbnb Base TypeScript Config
+  ...configs.base.typescript,
+  // Airbnb Next TypeScript Config
+  ...configs.next.typescript,
+  // Typescript Recommended Config
+  ...tsConfigs.recommended,
+  // Disable TypeScript rules for js files
+  {
+    files: ['**/*.{js,cjs,mjs}'],
+    ...configs.disableTypeChecked,
+  },
+];
+
+export default [
+  // Ignore .gitignore files/folder in eslint
+  includeIgnoreFile(gitignorePath),
+  // Airbnb Base Recommended Config
+  ...configs.base.recommended,
+  // Next Config
+  ...nextConfig,
+  // TypeScript Config
+  ...typescriptConfig,
+];
