b32933ebd64c263172353c52fbffa39b59dad220
Nisharg Shah <nishargshah3101@gmail.com>
2025-04-18 03:01:52 +0530

    feat: stylistic disable legacy added

--- packages/eslint-config-airbnb-extended/configs/base/typescript.ts
+++ packages/eslint-config-airbnb-extended/configs/base/typescript.ts
@@ -1,3 +1,4 @@
+import stylistic from '@stylistic/eslint-plugin';
 import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
 
 import typescriptConfig from '@/configs/typescript/config';
@@ -6,6 +7,10 @@ import type { Linter } from 'eslint';
 
 const baseTypescriptConfig = [
   ...Object.values(typescriptConfig),
+  {
+    name: 'airbnb/config/base-typescript-disable-legacy-stylistic',
+    ...stylistic.configs['disable-legacy'],
+  },
   {
     name: 'airbnb/config/base-typescript-import-x',
     settings: {
