bb1348f0d876b7f031e8e27104f15bc1873eb155
Nisharg Shah <nishargshah3101@gmail.com>
2025-04-19 01:24:18 +0530

    fix: react-stylistic added

--- packages/eslint-config-airbnb-extended/configs/react/recommended.ts
+++ packages/eslint-config-airbnb-extended/configs/react/recommended.ts
@@ -21,6 +21,8 @@ const reactRecommendedConfig = [
   {
     name: 'airbnb/config/react-configurations',
     rules: {
+      // disallow dangling underscores in identifiers
+      // https://eslint.org/docs/latest/rules/no-underscore-dangle
       // Allow Redux devtools variable
       'no-underscore-dangle': [
         dangleRules[0],
@@ -30,12 +32,9 @@ const reactRecommendedConfig = [
         },
       ],
 
-      // Specify whether double or single quotes should be used in JSX attributes
-      // https://eslint.org/docs/rules/jsx-quotes
-      // @deprecated
-      'jsx-quotes': ['error', 'prefer-double'],
-
-      // Class Methods can be used in react
+      // enforce that class methods use "this"
+      // https://eslint.org/docs/rules/class-methods-use-this
+      // Class Methods can be used in React
       'class-methods-use-this': [
         'error',
         {
@@ -59,6 +58,14 @@ const reactRecommendedConfig = [
           ],
         },
       ],
+    },
+  },
+  {
+    name: 'airbnb/config/react-stylistic',
+    rules: {
+      // specify whether double or single quotes should be used in JSX attributes
+      // https://eslint.style/rules/js/jsx-quotes
+      '@stylistic/jsx-quotes': ['error', 'prefer-double'],
     },
   },
 ] satisfies Linter.Config[];
