806d060d6cdd63bf3e7178a3132b6df6fd535078
Nisharg Shah <nishargshah3101@gmail.com>
2025-04-19 01:05:26 +0530

    feat: added stylisticPlus in typescript

--- packages/eslint-config-airbnb-extended/configs/typescript/config.ts
+++ packages/eslint-config-airbnb-extended/configs/typescript/config.ts
@@ -2,12 +2,14 @@ import typescriptBaseRules from '@/rules/typescript/typescriptBase';
 import typescriptEslintRules from '@/rules/typescript/typescriptEslint';
 import typescriptImportsRules from '@/rules/typescript/typescriptImports';
 import typescriptStylisticRules from '@/rules/typescript/typescriptStylistic';
+import typescriptStylisticPlusRules from '@/rules/typescript/typescriptStylisticPlus';
 
 import type { Linter } from 'eslint';
 
 const typescriptConfig = {
   base: typescriptBaseRules,
   typescriptStylistic: typescriptStylisticRules,
+  typescriptStylisticPlus: typescriptStylisticPlusRules,
   typescriptEslint: typescriptEslintRules,
   imports: typescriptImportsRules,
 } satisfies Record<string, Linter.Config>;
