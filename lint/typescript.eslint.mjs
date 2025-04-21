013436a5396db0f8bce6a3d8e393addc2fdacc7c
Nisharg Shah <nishargshah3101@gmail.com>
2025-04-22 02:54:51 +0530

    fix: own config based on the changes

--- lint/typescript.eslint.mjs
+++ lint/typescript.eslint.mjs
@@ -1,18 +1,12 @@
 import { configs, plugins, rules } from 'eslint-config-airbnb-extended';
 
 const customTSESLintConfig = [
+  // Typescript ESLint Plugin
   plugins.typescriptEslint,
   // AIRBNB BASE TYPESCRIPT CONFIG
   ...configs.base.typescript,
   // AIRBNB EXTENDED TYPESCRIPT CONFIG
   rules.typescript.typescriptEslintStrict,
-  // TYPESCRIPT CONFIG RULES
-  {
-    name: 'x/typescript-eslint/rules',
-    rules: {
-      '@typescript-eslint/unified-signatures': 'error',
-    },
-  },
   {
     name: 'x/import-x/disable-extraneous-deps',
     files: ['packages/create-airbnb-x-config/templates/**/eslint.config.mjs'],
