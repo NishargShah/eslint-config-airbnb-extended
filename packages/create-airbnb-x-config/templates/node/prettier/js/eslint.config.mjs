4549c5cf734b7e8fc6727526345c6b289e384761
Nisharg Shah <nishargshah3101@gmail.com>
2025-04-22 23:21:00 +0530

    feat: prettier templates added

---
+++ packages/create-airbnb-x-config/templates/node/prettier/js/eslint.config.mjs
@@ -0,0 +1,48 @@
+/**
+ * THIS FILE WAS AUTO-GENERATED.
+ * PLEASE DO NOT EDIT IT MANUALLY.
+ * ===============================
+ * IF YOU'RE COPYING THIS INTO AN ESLINT CONFIG, REMOVE THIS COMMENT BLOCK.
+ */
+
+import path from 'node:path';
+
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import { configs, plugins } from 'eslint-config-airbnb-extended';
+import prettierConfig from 'eslint-plugin-prettier/recommended';
+
+export const projectRoot = path.resolve('.');
+export const gitignorePath = path.resolve(projectRoot, '.gitignore');
+
+const jsConfig = [
+  // ESLINT RECOMMENDED RULES
+  {
+    name: 'js/config',
+    ...js.configs.recommended,
+  },
+  // Stylistic Plugin
+  plugins.stylistic,
+  // Import X Plugin
+  plugins.importX,
+  // Airbnb Base Recommended Config
+  ...configs.base.recommended,
+];
+
+const nodeConfig = [
+  // Node Plugin
+  plugins.node,
+  // Airbnb Node Recommended Config
+  ...configs.node.recommended,
+];
+
+export default [
+  // Ignore .gitignore files/folder in eslint
+  includeIgnoreFile(gitignorePath),
+  // Javascript Config
+  ...jsConfig,
+  // Node Config
+  ...nodeConfig,
+  // Prettier Config
+  prettierConfig,
+];
---
+++ packages/create-airbnb-x-config/templates/react/prettier/ts/eslint.config.mjs
@@ -0,0 +1,63 @@
+/**
+ * THIS FILE WAS AUTO-GENERATED.
+ * PLEASE DO NOT EDIT IT MANUALLY.
+ * ===============================
+ * IF YOU'RE COPYING THIS INTO AN ESLINT CONFIG, REMOVE THIS COMMENT BLOCK.
+ */
+
+import path from 'node:path';
+
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import { configs, plugins } from 'eslint-config-airbnb-extended';
+import prettierConfig from 'eslint-plugin-prettier/recommended';
+
+export const projectRoot = path.resolve('.');
+export const gitignorePath = path.resolve(projectRoot, '.gitignore');
+
+const jsConfig = [
+  // ESLINT RECOMMENDED RULES
+  {
+    name: 'js/config',
+    ...js.configs.recommended,
+  },
+  // Stylistic Plugin
+  plugins.stylistic,
+  // Import X Plugin
+  plugins.importX,
+  // Airbnb Base Recommended Config
+  ...configs.base.recommended,
+];
+
+const reactConfig = [
+  // React Plugin
+  plugins.react,
+  // React Hooks Plugin
+  plugins.reactHooks,
+  // React JSX-A11y Plugin
+  plugins.reactA11y,
+  // Airbnb React Recommended Config
+  ...configs.react.recommended,
+];
+
+const typescriptConfig = [
+  // Typescript ESLint Plugin
+  plugins.typescriptEslint,
+  // Airbnb Base TypeScript Config
+  ...configs.base.typescript,
+  // Airbnb React TypeScript Config
+  ...configs.react.typescript,
+];
+
+export default [
+  // Ignore .gitignore files/folder in eslint
+  includeIgnoreFile(gitignorePath),
+  // Javascript Config
+  ...jsConfig,
+  // React Config
+  ...reactConfig,
+  // TypeScript Config
+  ...typescriptConfig,
+  // Prettier Config
+  prettierConfig,
+];
