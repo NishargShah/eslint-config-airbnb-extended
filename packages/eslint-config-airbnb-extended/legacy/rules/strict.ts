a635f7040625685cc8c77fa23e90a478a70daa01
Nisharg Shah <nishargshah3101@gmail.com>
2025-05-31 00:57:19 +0530

    fix: changed strict

--- packages/eslint-config-airbnb-extended/legacy/rules/strict.ts
+++ packages/eslint-config-airbnb-extended/legacy/rules/strict.ts
@@ -5,6 +5,10 @@ import type { Linter } from 'eslint';
 const legacyStrictRules = {
   ...strictRules,
   name: `${strictRules.name}/legacy`,
+  rules: {
+    // babel inserts `'use strict';` for us
+    strict: ['error', 'never'],
+  },
 } satisfies Linter.Config;
 
 export default legacyStrictRules;
