5eff59cb9c060af949f492a72b3459b3fb38e80d
Nisharg Shah <nishargshah3101@gmail.com>
2025-05-31 18:01:32 +0530

    feat: react jsx a11y added

--- packages/eslint-config-airbnb-extended/legacy/rules/react/reactJsxA11y.ts
+++ packages/eslint-config-airbnb-extended/legacy/rules/react/reactJsxA11y.ts
@@ -1,20 +1,18 @@
-import { allFiles } from '@/utils';
+import reactJsxA11yRules from '@/rules/react/reactJsxA11y';
 
 import type { Linter } from 'eslint';
 
-const reactJsxA11yRules = {
-  name: 'airbnb/config/react-jsx-a11y',
-  files: allFiles,
-  languageOptions: {
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-  },
+const legacyReactJsxA11yRules = {
+  ...reactJsxA11yRules,
+  name: `${reactJsxA11yRules.name}/legacy`,
   rules: {
+    // ensure emoji are accessible
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/accessible-emoji.md
+    // disabled; rule is deprecated
+    'jsx-a11y/accessible-emoji': 'off',
+
     // Enforce that all elements that require alternative text have meaningful information
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/alt-text.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/alt-text.md
     'jsx-a11y/alt-text': [
       'error',
       {
@@ -26,12 +24,8 @@ const reactJsxA11yRules = {
       },
     ],
 
-    // Ensures anchor text is not ambiguous
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/anchor-ambiguous-text.md
-    'jsx-a11y/anchor-ambiguous-text': 'off',
-
     // Enforce that anchors have content
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/anchor-has-content.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-has-content.md
     'jsx-a11y/anchor-has-content': [
       'error',
       {
@@ -40,7 +34,7 @@ const reactJsxA11yRules = {
     ],
 
     // ensure <a> tags are valid
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/anchor-is-valid.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/0745af376cdc8686d85a361ce36952b1fb1ccf6e/docs/rules/anchor-is-valid.md
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
@@ -51,19 +45,19 @@ const reactJsxA11yRules = {
     ],
 
     // elements with aria-activedescendant must be tabbable
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/aria-activedescendant-has-tabindex.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/aria-activedescendant-has-tabindex.md
     'jsx-a11y/aria-activedescendant-has-tabindex': 'error',
 
     // Enforce all aria-* props are valid.
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/aria-props.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/aria-props.md
     'jsx-a11y/aria-props': 'error',
 
     // Enforce ARIA state and property values are valid.
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/aria-proptypes.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/aria-proptypes.md
     'jsx-a11y/aria-proptypes': 'error',
 
     // Require ARIA roles to be valid and non-abstract
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/aria-role.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/aria-role.md
     'jsx-a11y/aria-role': [
       'error',
       {
@@ -73,24 +67,24 @@ const reactJsxA11yRules = {
 
     // Enforce that elements that do not support ARIA roles, states, and
     // properties do not have those attributes.
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/aria-unsupported-elements.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/aria-unsupported-elements.md
     'jsx-a11y/aria-unsupported-elements': 'error',
 
     // Ensure the autocomplete attribute is correct and suitable for the form field it is used with
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/autocomplete-valid.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/29c68596b15c4ff0a40daae6d4a2670e36e37d35/docs/rules/autocomplete-valid.md
     'jsx-a11y/autocomplete-valid': [
-      'error',
+      'off',
       {
-        inputComponents: ['Input'],
+        inputComponents: [],
       },
     ],
 
     // require onClick be accompanied by onKeyUp/onKeyDown/onKeyPress
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/click-events-have-key-events.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/click-events-have-key-events.md
     'jsx-a11y/click-events-have-key-events': 'error',
 
     // Enforce that a control (an interactive element) has a text label.
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/control-has-associated-label.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/control-has-associated-label.md
     'jsx-a11y/control-has-associated-label': [
       'error',
       {
@@ -114,32 +108,32 @@ const reactJsxA11yRules = {
     ],
 
     // ensure <hX> tags have content and are not aria-hidden
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/heading-has-content.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/heading-has-content.md
     'jsx-a11y/heading-has-content': [
       'error',
       {
-        components: ['Typography', 'Text'],
+        components: [''],
       },
     ],
 
     // require HTML elements to have a "lang" prop
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/html-has-lang.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/html-has-lang.md
     'jsx-a11y/html-has-lang': 'error',
 
     // ensure iframe elements have a unique title
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/iframe-has-title.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/iframe-has-title.md
     'jsx-a11y/iframe-has-title': 'error',
 
     // Prevent img alt text from containing redundant words like "image", "picture", or "photo"
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/img-redundant-alt.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/img-redundant-alt.md
     'jsx-a11y/img-redundant-alt': 'error',
 
     // Elements with an interactive role and interaction handlers must be focusable
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/interactive-supports-focus.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/interactive-supports-focus.md
     'jsx-a11y/interactive-supports-focus': 'error',
 
     // Enforce that a label tag has a text label and an associated control.
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/label-has-associated-control.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/b800f40a2a69ad48015ae9226fbe879f946757ed/docs/rules/label-has-associated-control.md
     'jsx-a11y/label-has-associated-control': [
       'error',
       {
@@ -152,11 +146,11 @@ const reactJsxA11yRules = {
     ],
 
     // require HTML element's lang prop to be valid
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/lang.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/lang.md
     'jsx-a11y/lang': 'error',
 
     // media elements must have captions
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/media-has-caption.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/media-has-caption.md
     'jsx-a11y/media-has-caption': [
       'error',
       {
@@ -166,20 +160,16 @@ const reactJsxA11yRules = {
       },
     ],
 
-    // require that mouseover/outcome with focus/blur, for keyboard-only users
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/mouse-events-have-key-events.md
+    // require that mouseover/out come with focus/blur, for keyboard-only users
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/mouse-events-have-key-events.md
     'jsx-a11y/mouse-events-have-key-events': 'error',
 
     // Prevent use of `accessKey`
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/no-access-key.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-access-key.md
     'jsx-a11y/no-access-key': 'error',
 
-    // Enforce that aria-hidden="true" is not set on focusable elements.
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/no-aria-hidden-on-focusable.md
-    'jsx-a11y/no-aria-hidden-on-focusable': 'off',
-
     // prohibit autoFocus prop
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/no-autofocus.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-autofocus.md
     'jsx-a11y/no-autofocus': [
       'error',
       {
@@ -188,7 +178,7 @@ const reactJsxA11yRules = {
     ],
 
     // prevent distracting elements, like <marquee> and <blink>
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/no-distracting-elements.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-distracting-elements.md
     'jsx-a11y/no-distracting-elements': [
       'error',
       {
@@ -197,7 +187,7 @@ const reactJsxA11yRules = {
     ],
 
     // WAI-ARIA roles should not be used to convert an interactive element to non-interactive
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/no-interactive-element-to-noninteractive-role.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-interactive-element-to-noninteractive-role.md
     'jsx-a11y/no-interactive-element-to-noninteractive-role': [
       'error',
       {
@@ -206,7 +196,7 @@ const reactJsxA11yRules = {
     ],
 
     // A non-interactive element does not support event handlers (mouse and key handlers)
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/no-noninteractive-element-interactions.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-noninteractive-element-interactions.md
     'jsx-a11y/no-noninteractive-element-interactions': [
       'error',
       {
@@ -215,7 +205,7 @@ const reactJsxA11yRules = {
     ],
 
     // WAI-ARIA roles should not be used to convert a non-interactive element to interactive
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/no-noninteractive-element-to-interactive-role.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-noninteractive-element-to-interactive-role.md
     'jsx-a11y/no-noninteractive-element-to-interactive-role': [
       'error',
       {
@@ -228,7 +218,7 @@ const reactJsxA11yRules = {
     ],
 
     // Tab key navigation should be limited to elements on the page that can be interacted with.
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/no-noninteractive-tabindex.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-noninteractive-tabindex.md
     'jsx-a11y/no-noninteractive-tabindex': [
       'error',
       {
@@ -238,8 +228,12 @@ const reactJsxA11yRules = {
       },
     ],
 
+    // require onBlur instead of onChange
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-onchange.md
+    'jsx-a11y/no-onchange': 'off',
+
     // ensure HTML elements do not specify redundant ARIA roles
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/no-redundant-roles.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-redundant-roles.md
     'jsx-a11y/no-redundant-roles': [
       'error',
       {
@@ -248,7 +242,7 @@ const reactJsxA11yRules = {
     ],
 
     // Enforce that DOM elements without semantic behavior not have interaction handlers
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/no-static-element-interactions.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-static-element-interactions.md
     'jsx-a11y/no-static-element-interactions': [
       'error',
       {
@@ -256,46 +250,54 @@ const reactJsxA11yRules = {
       },
     ],
 
-    // Enforces using semantic DOM elements over the ARIA role property.
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/prefer-tag-over-role.md
-    'jsx-a11y/prefer-tag-over-role': 'off',
-
     // Enforce that elements with ARIA roles must have all required attributes
     // for that role.
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/role-has-required-aria-props.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/role-has-required-aria-props.md
     'jsx-a11y/role-has-required-aria-props': 'error',
 
     // Enforce that elements with explicit or implicit roles defined contain
     // only aria-* properties supported by that role.
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/role-supports-aria-props.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/role-supports-aria-props.md
     'jsx-a11y/role-supports-aria-props': 'error',
 
     // only allow <th> to have the "scope" attr
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/scope.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/scope.md
     'jsx-a11y/scope': 'error',
 
     // Enforce tabIndex value is not greater than zero.
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/tabindex-no-positive.md
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/tabindex-no-positive.md
     'jsx-a11y/tabindex-no-positive': 'error',
-  },
-} satisfies Linter.Config;
 
-export const deprecatedReactJsxA11yRules = {
-  name: 'airbnb/config/react-jsx-a11y/deprecated',
-  files: allFiles,
-  rules: {
-    // ensure emoji are accessible
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/accessible-emoji.md
-    'jsx-a11y/accessible-emoji': 'off',
+    // ----------------------------------------------------
+    // Rules that no longer exist in eslint-plugin-jsx-a11y
+    // ----------------------------------------------------
 
     // require that JSX labels use "htmlFor"
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/label-has-for.md
-    'jsx-a11y/label-has-for': 'off',
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-for.md
+    // deprecated: replaced by `label-has-associated-control` rule
+    'jsx-a11y/label-has-for': [
+      'off',
+      {
+        components: [],
+        required: {
+          every: ['nesting', 'id'],
+        },
+        allowChildren: false,
+      },
+    ],
 
-    // require onBlur instead of onChange
-    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/no-onchange.md
-    'jsx-a11y/no-onchange': 'off',
+    // Ensures anchor text is not ambiguous
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/93f78856655696a55309440593e0948c6fb96134/docs/rules/anchor-ambiguous-text.md
+    'jsx-a11y/anchor-ambiguous-text': 'off',
+
+    // Enforce that aria-hidden="true" is not set on focusable elements.
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/93f78856655696a55309440593e0948c6fb96134/docs/rules/no-aria-hidden-on-focusable.md
+    'jsx-a11y/no-aria-hidden-on-focusable': 'off',
+
+    // Enforces using semantic DOM elements over the ARIA role property.
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/93f78856655696a55309440593e0948c6fb96134/docs/rules/prefer-tag-over-role.md
+    'jsx-a11y/prefer-tag-over-role': 'off',
   },
 } satisfies Linter.Config;
 
-export default reactJsxA11yRules;
+export default legacyReactJsxA11yRules;
