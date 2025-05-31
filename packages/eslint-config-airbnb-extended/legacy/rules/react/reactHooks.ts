9f81f30c1202b8d2d849fb485caf664723330bb3
Nisharg Shah <nishargshah3101@gmail.com>
2025-05-31 18:01:21 +0530

    feat: react hooks added

--- packages/eslint-config-airbnb-extended/legacy/rules/react/reactHooks.ts
+++ packages/eslint-config-airbnb-extended/legacy/rules/react/reactHooks.ts
@@ -1,26 +1,19 @@
-import { allFiles } from '@/utils';
+import reactHooksRules from '@/rules/react/reactHooks';
 
 import type { Linter } from 'eslint';
 
-const reactHooksRules = {
-  name: 'airbnb/config/react-hooks',
-  files: allFiles,
-  languageOptions: {
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-  },
+const legacyReactHooksRules = {
+  ...reactHooksRules,
+  name: `${reactHooksRules.name}/legacy`,
   rules: {
-    // Verify the list of the dependencies for Hooks like useEffect and similar
-    // https://github.com/facebook/react/blob/main/packages/eslint-plugin-react-hooks/src/rules/ExhaustiveDeps.ts
-    'react-hooks/exhaustive-deps': 'error',
-
     // Enforce Rules of Hooks
-    // https://github.com/facebook/react/blob/main/packages/eslint-plugin-react-hooks/src/rules/RulesOfHooks.ts
+    // https://github.com/facebook/react/blob/c11015ff4f610ac2924d1fc6d569a17657a404fd/packages/eslint-plugin-react-hooks/src/RulesOfHooks.js
     'react-hooks/rules-of-hooks': 'error',
+
+    // Verify the list of the dependencies for Hooks like useEffect and similar
+    // https://github.com/facebook/react/blob/1204c789776cb01fbaf3e9f032e7e2ba85a44137/packages/eslint-plugin-react-hooks/src/ExhaustiveDeps.js
+    'react-hooks/exhaustive-deps': 'error',
   },
 } satisfies Linter.Config;
 
-export default reactHooksRules;
+export default legacyReactHooksRules;
