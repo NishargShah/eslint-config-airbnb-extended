fbe78fdce719c5b2cf3e12e5d4847a96fd56e369
Nisharg Shah <nishargshah3101@gmail.com>
2025-05-31 18:43:28 +0530

    fix: base recommended

--- packages/eslint-config-airbnb-extended/legacy/configs/base/recommended.ts
+++ packages/eslint-config-airbnb-extended/legacy/configs/base/recommended.ts
@@ -1,12 +1,9 @@
-import getImportSettings from '@/helpers/getImportSettings';
 import legacyBaseConfig from '@/legacy/configs/base/config';
-import legacyImportPluginConfig from '@/legacy/plugins/importPlugin';
-import { allFiles, jsFileWithoutReact } from '@/utils';
+import { allFiles } from '@/utils';
 
 import type { Linter } from 'eslint';
 
 const legacyBaseRecommendedConfig = [
-  legacyImportPluginConfig,
   ...Object.values(legacyBaseConfig),
   {
     name: 'airbnb/config/base-configurations/legacy',
@@ -17,16 +14,6 @@ const legacyBaseRecommendedConfig = [
         sourceType: 'module',
       },
     },
-    settings: {
-      'import/core-modules': [],
-      'import/ignore': ['node_modules', String.raw`\.(coffee|scss|css|less|hbs|svg|json)$`],
-    },
-  },
-  {
-    name: 'airbnb/config/base-settings-extensions-configurations/legacy',
-    files: jsFileWithoutReact,
-    // FIXME BUG
-    settings: getImportSettings({ javascript: true, typescript: false, jsx: false }),
   },
 ] satisfies Linter.Config[];
 
