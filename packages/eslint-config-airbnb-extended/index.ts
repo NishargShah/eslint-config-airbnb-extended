7e5bf0c5aea8856a416452f72ec447c0c9f67ab1
Nisharg Shah <nishargshah3101@gmail.com>
2025-06-17 23:29:33 +0530

    feat: extensions declared

--- packages/eslint-config-airbnb-extended/index.ts
+++ packages/eslint-config-airbnb-extended/index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import-x/no-rename-default, unicorn/prefer-export-from */
 
 import allConfigs from '@/configs';
+import allExtensions from '@/extensions';
 import allPlugins from '@/plugins';
 import allRules from '@/rules';
 
@@ -12,3 +13,5 @@ export const rules = allRules;
 export const configs = allConfigs;
 
 export const plugins = allPlugins;
+
+export const extensions = allExtensions;
